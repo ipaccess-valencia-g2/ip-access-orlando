cc5993d620d4b1d5e2e42b3d62b071a3fa4c8eb1
d-angelotorres <dangelotorres2@gmail.com>
2025-05-23 22:21:33 -0400

    fix(index): forgot to commit you last time, my bad king 😅

--- mobile/app/index.js
+++ mobile/app/index.js
@@ -1,9 +1,24 @@
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { useRouter } from 'expo-router';
 
 export default function HomeScreen() {
+  const router = useRouter();
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome to IP Access!</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white', // Add a light background color
+      }}
+    >
+      <Text style={{ color: 'black', fontSize: 20, marginBottom: 20 }}>
+        ConnectOrlando
+        </Text>
+      <Button title="Register" onPress={() => router.push('/register')} />
+      <Button title="Login" onPress={() => router.push('/login')} />
     </View>
   );
 }
