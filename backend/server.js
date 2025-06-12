5616adf93ad7ce263651489af284a5bfdf063c66
DisgruntledStudent <144858260+DisgruntledStudent@users.noreply.github.com>
2025-06-12 14:28:31 -0400

    3

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3307;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is listening on port ${PORT}`);
 });
